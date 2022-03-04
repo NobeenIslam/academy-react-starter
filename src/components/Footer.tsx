9068dda6c867b17ff55f457a2542cede22b2950e
Nobeen <n.islam.1@outlook.com>
2022-03-04 20:09:11 +0000

    Format Fotter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,22 @@
 import NavItem from "./NavItem";
 
-function Footer(): JSX.Element{
-    return(
-        <footer>
-            <NavItem label = {"Abdul"} scholarLink = {"https://my-fav-places.netlify.app/"}/>
-            <NavItem label = {"Laura"} scholarLink = {"https://hardcore-northcutt-fafb4e.netlify.app/"}/>
-            <NavItem label = {"Owen"} scholarLink = {"https://owen-fav-places.netlify.app/"}/>
-        </footer>
-    )
+function Footer(): JSX.Element {
+  return (
+    <footer>
+      <NavItem
+        label={"Abdul"}
+        scholarLink={"https://my-fav-places.netlify.app/"}
+      />
+      <NavItem
+        label={"Laura"}
+        scholarLink={"https://hardcore-northcutt-fafb4e.netlify.app/"}
+      />
+      <NavItem
+        label={"Owen"}
+        scholarLink={"https://owen-fav-places.netlify.app/"}
+      />
+    </footer>
+  );
 }
 
 export default Footer;
