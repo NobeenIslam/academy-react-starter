e7ec2ffb607cdd67ac95037be96b7538a93755a9
Nobeen <n.islam.1@outlook.com>
2022-03-04 20:06:50 +0000

    Fromate NavItem

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,16 +1,15 @@
 interface NavItemProps {
-    label: string;
-    scholarLink: string;
-  }
-  
-  function NavItem(props: NavItemProps): JSX.Element {
-    return (
-      <>
-        <span> | </span>
-        <a href={props.scholarLink}>~{props.label}</a>
-        
-      </>
-    );
-  }
-  
-  export default NavItem;
+  label: string;
+  scholarLink: string;
+}
+
+function NavItem(props: NavItemProps): JSX.Element {
+  return (
+    <>
+      <span> | </span>
+      <a href={props.scholarLink}>~{props.label}</a>
+    </>
+  );
+}
+
+export default NavItem;
