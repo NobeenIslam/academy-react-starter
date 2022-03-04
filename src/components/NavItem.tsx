e99d0d0db9215c8cdf79e95905d2bebecb8b3007
Nobeen <n.islam.1@outlook.com>
2022-03-04 19:52:54 +0000

    Create NavItem

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,16 @@
+interface NavItemProps {
+    label: string;
+    scholarLink: string;
+  }
+  
+  function NavItem(props: NavItemProps): JSX.Element {
+    return (
+      <>
+        <span> | </span>
+        <a href={props.scholarLink}>~{props.label}</a>
+        
+      </>
+    );
+  }
+  
+  export default NavItem;
