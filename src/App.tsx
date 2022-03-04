f25861e3a8b62aab1572081019b5d5778427acb5
Nobeen <n.islam.1@outlook.com>
2022-03-04 20:35:44 +0000

    Add Header comp to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App(): JSX.Element {
   return (
     <>
+      <Header />
       <MainContent />
       <Footer />
     </>
