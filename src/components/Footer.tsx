c530d1a17214fddd78bac07d1a4dfca6685172eb
Nobeen <n.islam.1@outlook.com>
2022-03-04 19:57:53 +0000

    Create footer and population with Abdul, Laura and Owen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,13 @@
-export default {}
+import NavItem from "./NavItem";
+
+function Footer(): JSX.Element{
+    return(
+        <footer>
+            <NavItem label = {"Abdul"} scholarLink = {"https://my-fav-places.netlify.app/"}/>
+            <NavItem label = {"Laura"} scholarLink = {"https://hardcore-northcutt-fafb4e.netlify.app/"}/>
+            <NavItem label = {"Owen"} scholarLink = {"https://owen-fav-places.netlify.app/"}/>
+        </footer>
+    )
+}
+
+export default Footer;
