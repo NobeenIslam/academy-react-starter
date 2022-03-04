724125c7469fded9196b3724bb6cb27464d374ed
Nobeen <n.islam.1@outlook.com>
2022-03-04 19:29:36 +0000

    Added blank exports so typescript could compile

--- src/components/FavouritePlace.tsx
+++ src/components/FavouritePlace.tsx
@@ -1,19 +1,21 @@
-interface FavouritePlaceProps{
-    imagePath: string;
-    caption: string;
-    location: {city: string; country: string; link:string};
-    description: string;
+interface FavouritePlaceProps {
+  imagePath: string;
+  caption: string;
+  location: { city: string; country: string; mapLink: string };
+  description: string;
 }
 
-
-function FavouritePlace(props: FavouritePlaceProps): JSX.Element{
-    return (
-        <div>
-            <img src = {props.imagePath} alt =""/>
-            <p>{props.caption}</p>
-            <p>{props.location.city}, {props.location.country} (<a href = {props.location.link}>map link</a></p>
-        </div>
-    )
+function FavouritePlace(props: FavouritePlaceProps): JSX.Element {
+  return (
+    <div>
+      <img src={props.imagePath} alt="" />
+      <p>{props.caption}</p>
+      <p>
+        {props.location.city}, {props.location.country} (
+        <a href={props.location.mapLink}>map link</a>
+      </p>
+    </div>
+  );
 }
 
-export default FavouritePlace
+export default FavouritePlace;
